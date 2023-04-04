619be7a4be7a7849376e52d083f2864240bd0cef
lillyphan <lphan24@kentdenver.org>
2023-04-03 20:21:32 -0600

    Add generator

--- src/MarkovChains.java
+++ src/MarkovChains.java
@@ -15,9 +15,10 @@ public class MarkovChains<Key, Value> {
         String[] words;
         ArrayList<String> beginners = new ArrayList<>();
         fill(beginners);
-        ArrayList<String> end = new ArrayList<>();
-        end.add(".");
-        end.add("!");
+        ArrayList<String> endPunc = new ArrayList<>();
+        endPunc.add(".");
+        endPunc.add("!");
+        ArrayList<String> endWords = new ArrayList<>();
 
         System.out.println("Enter file name: ");
         fileName = s.nextLine();
@@ -28,12 +29,35 @@ public class MarkovChains<Key, Value> {
             for (int i = 0; i < words.length - 1; i++){
                 if (!hm.containsKey(words[i])) {
                     hm.put(words[i], new ArrayList<String>());
+                } else if (!(words[i + 1].charAt(0) >= 65 && words[i + 1].charAt(0) <= 90)) {
+                    hm.get(words[i]).add(words[i + 1]);
                 }
-                hm.get(words[i]).add(words[i + 1]);
             }
         }
 
-        System.out.println(Arrays.asList(hm));
+        System.out.println("Enter in the number of words you would like to generate: ");
+        int numWords = s.nextInt();
+
+        String YAY = "";
+        YAY = YAY.concat(beginners.get(r.nextInt(beginners.size())));
+        String currWord = YAY.substring(0, YAY.length());
+
+        for (int i = 0; i < numWords - 1; i++){
+            if (hm.get(currWord) == null || hm.get(currWord).isEmpty()){
+                int random = r.nextInt(beginners.size());
+                YAY = YAY.concat(endPunc.get(r.nextInt(2)) + "\n");
+                YAY = YAY.concat(" " + beginners.get(random));
+                currWord = beginners.get(random);
+            }
+            System.out.println(hm.get(currWord).size());
+            int random = r.nextInt(hm.get(currWord).size());
+            YAY = YAY + " " + hm.get(currWord).get(random);
+            currWord = hm.get(currWord).get(random);
+        }
+
+        YAY = YAY.concat(endPunc.get(r.nextInt(2)));
+
+        System.out.println(YAY);
 
 //        1. Ask the user for a file name. This file should be a text file for the program to train on.
 //
@@ -59,10 +83,8 @@ public class MarkovChains<Key, Value> {
         Scanner oldScanner = new Scanner(new File(file));
         FileWriter fw = new FileWriter("nf.txt");
         while (oldScanner.hasNextLine()){
-//            System.out.println(oldScanner.nextLine());
             String s = oldScanner.nextLine().replaceAll("'", "3").replaceAll("-", "4");
             String s2 = s.replaceAll("\\p{Punct}", "");
-//            fw.write(s);
             fw.write(s2.replaceAll("3", "'").replaceAll("4", "-"));
             fw.write("\n");
         }
@@ -71,23 +93,21 @@ public class MarkovChains<Key, Value> {
     }
 
     private static void fill(ArrayList<String> array){
-        array.add("She");
-        array.add("I");
-        array.add("The");
-        array.add("A");
-        array.add("She");
-        array.add("Their");
-        array.add("We");
-        array.add("When");
-        array.add("There");
-        array.add("And");
-        array.add("They");
-        array.add("All");
-        array.add("He");
-        array.add("His");
-        array.add("It");
-        array.add("My");
-        array.add("In");
-        array.add("At");
+//        if (beginner){
+            array.add("She");
+            array.add("I");
+            array.add("The");
+            array.add("She");
+            array.add("When");
+            array.add("And");
+            array.add("They");
+            array.add("All");
+            array.add("He");
+            array.add("It");
+            array.add("My");
+            array.add("In");
+            array.add("There");
+            array.add("This");
+//        }
     }
 }
